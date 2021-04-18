da49ab2b60c4c41432d9e3bb7e135d1e7c78bd8c
ShiunMurata <purpleclouds0916@gmail.com>
2021-04-18 13:32:58 +0900

    データベースの変更によるモデル制限の変更

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,12 +2,12 @@
 #
 # Table name: urls
 #
-#  id             :integer          not null, primary key
-#  external_url   :string
-#  movie_url      :string
-#  name           :string
-#  teacher_hp_url :string
-#  url            :string
+#  id             :bigint           not null, primary key
+#  external_url   :string(255)
+#  movie_url      :string(255)
+#  name           :string(255)
+#  teacher_hp_url :string(255)
+#  url            :string(255)
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
 #  post_id        :integer
