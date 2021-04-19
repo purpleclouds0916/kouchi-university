d991dece66c3a3cf12dd69482d5cfaddfca90486
ShiunMurata <purpleclouds0916@gmail.com>
2021-04-19 12:57:37 +0900

    Revert "データベース変更による型の変更"
    This reverts commit 27074aa1b3cb7fb1a880ee764b6af943c16c1e3e.

--- db/migrate/20210418032200_rename_blob_id_column_to_active_storage_attachments.rb
+++
@@ -1,5 +0,0 @@
-class RenameBlobIdColumnToActiveStorageAttachments < ActiveRecord::Migration[6.0]
-  def change
-    rename_column :active_storage_attachments, :blob_id, :bigint
-  end
-end
--- db/migrate/20210418041235_change_datatype_job_description_of_alumnis.rb
+++
@@ -1,5 +0,0 @@
-class ChangeDatatypeJobDescriptionOfAlumnis < ActiveRecord::Migration[6.0]
-  def change
-    change_column :alumnis, :job_description, :text
-  end
-end
