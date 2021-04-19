2c756851baac5f3a93313fce3a931b26751b74e6
ShiunMurata <purpleclouds0916@gmail.com>
2021-04-19 13:39:27 +0900

    データベースの制限による変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_18_041235) do
+ActiveRecord::Schema.define(version: 2021_04_19_023159) do
 
   create_table "action_text_rich_texts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
+    t.string "name", limit: 255, null: false
     t.text "body", size: :long
-    t.string "record_type", null: false
+    t.string "record_type", limit: 255, null: false
     t.bigint "record_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -23,36 +23,35 @@ ActiveRecord::Schema.define(version: 2021_04_18_041235) do
   end
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
+    t.string "name", limit: 255, null: false
+    t.string "record_type", limit: 255, null: false
     t.bigint "record_id", null: false
-    t.bigint "bigint", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
-    t.index ["bigint"], name: "index_active_storage_attachments_on_bigint"
-    t.index ["record_type", "record_id", "name", "bigint"], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
+    t.string "key", limit: 255, null: false
+    t.string "filename", limit: 255, null: false
+    t.string "content_type", limit: 255
     t.text "metadata"
     t.bigint "byte_size", null: false
-    t.string "checksum", null: false
+    t.string "checksum", limit: 255, null: false
     t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "alumnis", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "job"
-    t.string "birthplace"
+    t.string "name", limit: 255
+    t.string "job", limit: 255
+    t.string "birthplace", limit: 255
     t.text "job_description"
     t.text "original_content"
-    t.string "original_title"
+    t.string "original_title", limit: 255
     t.text "reason"
-    t.string "research_field"
-    t.string "research_office"
+    t.string "research_field", limit: 255
+    t.string "research_office", limit: 255
     t.text "learning"
     t.text "memories"
     t.datetime "created_at", precision: 6, null: false
@@ -61,21 +60,21 @@ ActiveRecord::Schema.define(version: 2021_04_18_041235) do
   end
 
   create_table "inquiries", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "message"
+    t.string "name", limit: 255
+    t.string "message", limit: 255
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "email"
-    t.string "kinds"
+    t.string "email", limit: 255
+    t.string "kinds", limit: 255
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "title"
+    t.string "title", limit: 255
     t.text "content"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "lead"
+    t.string "lead", limit: 255
     t.integer "tag_id"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
@@ -89,43 +88,43 @@ ActiveRecord::Schema.define(version: 2021_04_18_041235) do
   end
 
   create_table "tags", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
+    t.string "name", limit: 255
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "teachers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "job"
+    t.string "name", limit: 255
+    t.string "job", limit: 255
     t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "urls", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "url"
+    t.string "name", limit: 255
+    t.string "url", limit: 255
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "post_id"
-    t.string "movie_url"
-    t.string "teacher_hp_url"
-    t.string "external_url"
+    t.string "movie_url", limit: 255
+    t.string "teacher_hp_url", limit: 255
+    t.string "external_url", limit: 255
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
+    t.string "name", limit: 255
+    t.string "email", limit: 255
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "password_digest"
-    t.string "remember_digest"
+    t.string "password_digest", limit: 255
+    t.string "remember_digest", limit: 255
     t.boolean "admin", default: false
-    t.string "reset_digest"
+    t.string "reset_digest", limit: 255
     t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "bigint"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "posts", "users"
 end
--- test/models/skill_test.rb
+++ test/models/skill_test.rb
@@ -3,7 +3,7 @@
 # Table name: skills
 #
 #  id         :bigint           not null, primary key
-#  name       :string(255)
+#  name       :string(191)
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #  post_id    :integer
