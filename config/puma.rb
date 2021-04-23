dd95067dc9bd2c86ac074eb29fc6efb83865f47a
ShiunMurata <purpleclouds0916@gmail.com>
2021-04-23 11:27:52 +0900

    本番環境用に変更

--- config/puma.rb
+++ config/puma.rb
@@ -1,7 +1,8 @@
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
-port        ENV.fetch("PORT") { 3000 }
+bind "unix://#{Rails.root}/tmp/sockets/puma.sock"
+#port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { ENV['RACK_ENV'] || "production" }
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 workers ENV.fetch("WEB_CONCURRENCY") { 2 }
