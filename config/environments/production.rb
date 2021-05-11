089b8142676da7e0fb03eb18b8940eaa93899e36
EC2 Default User <ec2-user@ip-10-0-0-225.ap-northeast-1.compute.internal>
2021-05-11 08:39:15 +0000

    デプロイのための環境設定の変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
@@ -112,7 +112,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
-  config.force_ssl = true
+  #config.force_ssl = true
   
   config.action_mailer.raise_delivery_errors = true
   
